98921f4c4c987c73d6239f9ea001f0b139a43f22
xujunjie <xjj0906@vip.qq.com>
2015-09-08 23:07:58 +0800

    加密保存密码

--- RadminSavePassword/Config/ServerInfo.cs
+++ RadminSavePassword/Config/ServerInfo.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace RadminSavePassword
 {
     [Serializable]
-    public class ServerInfo
+    public class ServerInfo : ISerializable
     {
+        private readonly EntryptDecrypt _entryptDecrypt = new EntryptDecrypt();
+
         public string Name { get; set; }
+
         public string UserName { get; set; }
+
         public string Password { get; set; }
+
+        public ServerInfo()
+        {
+        }
+
+        protected ServerInfo(SerializationInfo info, StreamingContext context)
+        {
+            Name = info.GetString("Name");
+            UserName = info.GetString("UserName");
+
+            string decryptPassword = _entryptDecrypt.Decrypt(info.GetString("Password"));
+            Password = decryptPassword;
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("UserName", UserName);
+
+            string entryptPassword = _entryptDecrypt.Encrypt(Password);
+            info.AddValue("Password", entryptPassword);
+        }
     }
 }
---
+++ RadminSavePassword/EntryptDecrypt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RadminSavePassword
+{
+    public class EntryptDecrypt
+    {
+        const string Key = "~x1o5*7%";//8位key
+
+        public string Encrypt(string pToEncrypt)
+        {
+            if (string.IsNullOrEmpty(pToEncrypt))
+                return string.Empty;
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(Key);
+                byte[] rgbIV = Encoding.UTF8.GetBytes(Key);
+                byte[] inputByteArray = Encoding.UTF8.GetBytes(pToEncrypt);
+                DESCryptoServiceProvider dcsp = new DESCryptoServiceProvider();
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, dcsp.CreateEncryptor(rgbKey, rgbIV), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                        cStream.FlushFinalBlock();
+                        return Convert.ToBase64String(mStream.ToArray());
+                    }
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        public string Decrypt(string pToDecrypt)
+        {
+            if (string.IsNullOrEmpty(pToDecrypt))
+                return string.Empty;
+            try
+            {
+                byte[] rgbKey = Encoding.UTF8.GetBytes(Key);
+                byte[] rgbIV = Encoding.UTF8.GetBytes(Key);
+                byte[] inputByteArray = Convert.FromBase64String(pToDecrypt);
+                DESCryptoServiceProvider dcsp = new DESCryptoServiceProvider();
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, dcsp.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(inputByteArray, 0, inputByteArray.Length);
+                        cStream.FlushFinalBlock();
+                        return Encoding.UTF8.GetString(mStream.ToArray());
+                    }
+                }
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        } 
+    }
+}
